44c8237383c1294fe5f2b14352dc7ff075cf9ff7
meenal <meenallepakshi@gmail.com>
2026-04-28 06:26:49 +0530

    resolved merge conflicts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // global styles (important)
+import './index.css';
 
 const rootElement = document.getElementById('root');
 
